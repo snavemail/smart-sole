b1a1ea37d4d25c218c3a20a25eb4f8c748d6d59a
snavemail <98936310+snavemail@users.noreply.github.com>
2024-03-01 00:31:34 -0500

    Add blehook

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useBle } from './hooks/useBle';
 import './App.css';
 
 function App() {
+  const { connect, isConnected, data } = useBle();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      {isConnected ? <h1>Connected {data}</h1> : <button onClick={connect}>Connect</button>}
     </div>
   );
 }
---
+++ src/hooks/useBle.tsx
@@ -0,0 +1,111 @@
+// import * as React from 'react';
+// export interface BLE {
+//   connect: () => void;
+//   isConnected: boolean;
+//   data: string[];
+// }
+
+// export const useBle = (): BLE => {
+//   const [isConnected, setIsConnected] = React.useState(false);
+//   const [data, setData] = React.useState<string[]>([]);
+
+//   const connect = async () => {
+//     const device = await navigator.bluetooth.requestDevice({
+//       filters: [{ services: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e'] }],
+//     });
+//     const server = await device.gatt?.connect();
+
+//     if (!server) {
+//       return;
+//     }
+
+//     const service = await server.getPrimaryService('6e400001-b5a3-f393-e0a9-e50e24dcca9e');
+
+//     const serviceData = await service.getCharacteristic(
+//       '6e400001-b5a3-f393-e0a9-e50e24dcca9e', // Philips Hue Light On/Off Toggle
+//     );
+
+//     setData([serviceData.uuid]);
+
+//     setIsConnected(true);
+//   };
+
+//   return { connect, isConnected, data };
+// };
+
+import * as React from 'react';
+
+export interface BLE {
+  connect: () => void;
+  disconnect: () => void;
+  isConnected: boolean;
+  data: string[];
+}
+
+export const useBle = (): BLE => {
+  const [isConnected, setIsConnected] = React.useState(false);
+  const [data, setData] = React.useState<string[]>([]);
+  const [characteristic, setCharacteristic] =
+    React.useState<BluetoothRemoteGATTCharacteristic | null>(null);
+
+  const connect = async () => {
+    console.log('Requesting Bluetooth Device...');
+    try {
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [{ services: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e'] }],
+      });
+      console.log('Device:', device);
+      const server = await device.gatt?.connect();
+      console.log('Server:', server);
+
+      if (!server) {
+        console.error('No server');
+        return;
+      }
+
+      const service = await server.getPrimaryService('6e400001-b5a3-f393-e0a9-e50e24dcca9e');
+      console.log('Service:', service);
+      const characteristic = await service.getCharacteristic(
+        '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
+      );
+      console.log('Characteristic:', characteristic);
+
+      characteristic.addEventListener(
+        'characteristicvaluechanged',
+        handleCharacteristicValueChanged,
+      );
+      await characteristic.startNotifications();
+
+      setCharacteristic(characteristic);
+      setIsConnected(true);
+    } catch (error) {
+      console.error('Bluetooth connection error:', error);
+      setIsConnected(false);
+    }
+  };
+
+  const disconnect = () => {
+    if (characteristic) {
+      characteristic.removeEventListener(
+        'characteristicvaluechanged',
+        handleCharacteristicValueChanged,
+      );
+      characteristic.stopNotifications();
+    }
+    setIsConnected(false);
+  };
+
+  const handleCharacteristicValueChanged = (event: Event) => {
+    const value = (event.target as BluetoothRemoteGATTCharacteristic).value;
+    if (value) {
+      const decoder = new TextDecoder('utf-8');
+      const text = decoder.decode(value);
+      const newData = [...data, text];
+      setData(newData.slice(Math.max(newData.length - 14, 0)));
+    } else {
+      console.error('No value in characteristic');
+    }
+  };
+
+  return { connect, disconnect, isConnected, data };
+};
