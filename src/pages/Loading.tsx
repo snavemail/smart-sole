6376c4fe3081894931571a232d51899081cacd13
snavemail <liam.jk.evans@gmail.com>
2024-03-09 10:00:34 -0500

    Add dynamic loading text to Loading component

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -2,8 +2,20 @@ import React, { useEffect, useState } from 'react';
 import '../css/loading.css';
 import RightPrintIcon from '../icons/RightPrintIcon';
 import LeftPrintIcon from '../icons/LeftPrintIcon';
+import { loadingStrings } from '../utils';
+import { getRandomString } from '../utils/loading';
 
 export default function Loading() {
+  const [loadingText, setLoadingText] = useState('Loading');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLoadingText(getRandomString(loadingText, loadingStrings));
+      console.log('loadingText', loadingText);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [loadingText]);
+
   return (
     <div className='loading-screen'>
       <div className='left-foot foot-div'>
@@ -12,7 +24,17 @@ export default function Loading() {
       <div className='right-foot foot-div'>
         <RightPrintIcon />
       </div>
-      <div className='loading-text'>Loading...</div>
+      <div className='loading-container'>
+        <p className='loading-text'>
+          <span>{loadingText}</span>
+        </p>
+      </div>
+      <div className='next-div'>
+        <a href='/graph'>Next (Graph)</a>
+      </div>
+      <div className='next-div'>
+        <a href='/gif'>Next (GIF)</a>
+      </div>
     </div>
   );
 }
