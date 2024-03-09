80946b66e32fa43a7c8561b7204b68bc1b06df3f
snavemail <liam.jk.evans@gmail.com>
2024-03-09 06:40:17 -0500

    Add Loading page to routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Connect from './pages/Connect';
 import Home from './pages/Home';
 import { useBle } from './hooks/useBle';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Loading from './pages/Loading';
 
 function App() {
   const { isConnected } = useBle();
@@ -13,6 +14,7 @@ function App() {
         <Route index element={isConnected ? <Home /> : <Connect />} />
         <Route path='connect' element={<Home />} />
         <Route path='home' element={<Home />} />
+        <Route path='loading' element={<Loading />} />
         <Route path='/*' element={<div>404</div>} />
       </Routes>
     </Router>
