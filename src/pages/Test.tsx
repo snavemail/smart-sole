63450a5d5508697249125f0564009302765587c3
snavemail <liam.jk.evans@gmail.com>
2024-03-11 17:51:09 -0400

    Refactor sensor data handling and remove unused code

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -3,34 +3,29 @@ import '../css/home.css';
 import FeetIcon from '../icons/FeetIcon';
 import { getCircleStyles } from '../utils';
 import { SensorData } from '../types';
+import { useBle } from '../hooks/useBle';
 
 export default function Test() {
+  const { data } = useBle();
   const [leftSensorData, setLeftSensorData] = useState<SensorData>({
-    timestamp: new Date().getHours(),
-    sensorValues: Array(6).fill(0),
-  });
-
-  const [rightSensorData, setRightSensorData] = useState<SensorData>({
-    timestamp: 0,
+    timestamp: new Date().getTime(),
     sensorValues: Array(6).fill(0),
   });
 
   useEffect(() => {
     const interval = setInterval(() => {
-      const newTimestamp = new Date().getHours();
+      const newTimestamp = new Date().getTime();
 
       const newLeftSensorValues = leftSensorData.sensorValues.map(() => Math.random() * 1000);
-
-      const newRightSensorValues = rightSensorData.sensorValues.map(() => Math.random() * 1000);
-
-      setRightSensorData({ timestamp: newTimestamp, sensorValues: newRightSensorValues });
 
       setLeftSensorData({ timestamp: newTimestamp, sensorValues: newLeftSensorValues });
     }, 1000);
     return () => clearInterval(interval);
-  }, [leftSensorData.sensorValues, rightSensorData.sensorValues]);
+  }, [leftSensorData.sensorValues]);
 
   const maxValue = 1000;
+
+  const sensorData = data.sensorValues.length > 0 ? data : leftSensorData;
 
   return (
     <div className='home-wrapper'>
@@ -40,14 +35,14 @@ export default function Test() {
         </h1>
       </div>
       <div className='foot-div'>
+        <div className='sensor-container timestamp'>{sensorData.timestamp}</div>
         <FeetIcon />
-        {leftSensorData.sensorValues.map((value, index) => (
+        {sensorData.sensorValues.map((value, index) => (
           <div className={`sensor-container sensor-left-${index}`}>
             {value.toFixed(2)}
             <div className='sensor-circle' style={getCircleStyles(value, maxValue)} />
           </div>
         ))}
-        <div className='sensor-container timestamp'>{leftSensorData.timestamp}</div>
       </div>
       <div className='next-div'>
         <a href='/loading'>Next</a>
