b01f9e4edc390403aedc0601f77eacc1c9fa61b4
snavemail <liam.jk.evans@gmail.com>
2024-03-13 06:57:33 -0400

    Add test-related functionality and UI improvements

--- frontend/src/pages/User.tsx
+++ frontend/src/pages/User.tsx
@@ -1,24 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { Test, User } from '../types';
+import { Profile, Test, User } from '../types';
 import Loading from './Loading';
 import '../css/user-details.css';
+import TestComponent from '../components/Test';
 
 export default function UserDetails() {
   const { userId } = useParams();
   const [user, setUser] = useState<User | null>(null);
+  const [profile, setProfile] = useState<Profile | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [userTests, setUserTests] = useState<Test[]>([]);
+  const [id, setId] = useState<number>(0);
+
+  const handleTestClick = (testId: number) => {
+    setId(testId);
+  };
 
   useEffect(() => {
-    const fetchUser = async () => {
+    const fetchData = async () => {
       setLoading(true);
-      const response = await axios.get(`http://127.0.0.1:8000/api/users/${userId}`);
-      setUser(response.data);
-      setLoading(false);
+      try {
+        const userResponse = await axios.get(`http://127.0.0.1:8000/api/users/${userId}`);
+        setUser(userResponse.data);
+        const profileResponse = await axios.get(
+          `http://127.0.0.1:8000/api/get-user-profile/${userResponse.data.id}`,
+        );
+        setProfile(profileResponse.data);
+
+        const testsResponse = await axios.get(
+          `http://127.0.0.1:8000/api/get-user-tests/${profileResponse.data.id}`,
+        );
+        setUserTests(testsResponse.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
     };
-    fetchUser();
+    if (userId) {
+      fetchData();
+    }
   }, [userId]);
 
   if (loading) {
@@ -39,14 +62,25 @@ export default function UserDetails() {
             </span>
           </h1>
         </div>
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
+        <div className='test-wrapper'>
+          {userTests
+            .sort((a, b) => new Date(b.start_time).getTime() - new Date(a.start_time).getTime())
+            .map(test => (
+              <button
+                className={`test-button ${test.id === id ? `test-selected` : ''}`}
+                onClick={() => handleTestClick(test.id)}>
+                <TestComponent key={test.id} test={test} />
+              </button>
+            ))}
+        </div>
+        <div className='button-container'>
+          <button className='start-test-button'>Start Test</button>
+        </div>
       </div>
       <div className='content'>
-        {/* Main content area */}
-        <h1>Content Area</h1>
+        <h1>Graphs</h1>
         <p>This is the main content area of the page.</p>
+        <p>{id}</p>
       </div>
     </div>
   );
