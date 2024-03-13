90ecb5b59a57acce77aa4686a57a41bbb4623997
snavemail <98936310+snavemail@users.noreply.github.com>
2024-03-12 22:57:21 -0400

    Update URLs for API endpoints

--- bleproject/bleapp/urls.py
+++ bleproject/bleapp/urls.py
@@ -1,10 +1,43 @@
-# urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("users", views.get_users),
-    path("user/<str:user_id>", views.get_user),
-    path("update_user/<str:user_id>", views.update_user),
-    path("delete_user/<str:user_id>", views.delete_user),
+    path("users/", views.UserListCreate.as_view(), name="user-list-create"),
+    path(
+        "users/<int:pk>/", views.UserRetrieveUpdateDestroy.as_view(), name="user-detail"
+    ),
+    path("profiles/", views.ProfileListCreate.as_view(), name="profile-list-create"),
+    path(
+        "profiles/<int:pk>/",
+        views.ProfileRetrieveUpdateDestroy.as_view(),
+        name="profile-detail",
+    ),
+    path("tests/", views.TestListCreate.as_view(), name="test-list-create"),
+    path(
+        "tests/<int:pk>/", views.TestRetrieveUpdateDestroy.as_view(), name="test-detail"
+    ),
+    path("steps/", views.StepListCreate.as_view(), name="step-list-create"),
+    path(
+        "steps/<int:pk>/", views.StepRetrieveUpdateDestroy.as_view(), name="step-detail"
+    ),
+    path(
+        "sensor-readings/",
+        views.SensorReadingListCreate.as_view(),
+        name="sensor-reading-list-create",
+    ),
+    path(
+        "sensor-readings/<int:pk>/",
+        views.SensorReadingRetrieveUpdateDestroy.as_view(),
+        name="sensor-reading-detail",
+    ),
+    path(
+        "average-steps/",
+        views.AverageStepListCreate.as_view(),
+        name="average-step-list-create",
+    ),
+    path(
+        "average-steps/<int:pk>/",
+        views.AverageStepRetrieveUpdateDestroy.as_view(),
+        name="average-step-detail",
+    ),
 ]
