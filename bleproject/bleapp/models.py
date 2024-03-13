f925a99510c0d791b07d595c3ecb03cd1656328b
snavemail <98936310+snavemail@users.noreply.github.com>
2024-03-12 22:56:52 -0400

    Refactor models.py to add new fields and update existing fields

--- bleproject/bleapp/models.py
+++ bleproject/bleapp/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.hashers import make_password, check_password
 # Create your models here.
 class User(models.Model):
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    # password = models.CharField(max_length=128)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -18,8 +18,23 @@ class User(models.Model):
         return check_password(raw_password, self.password)
 
 
-class Test(models.Model):
+class Profile(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    age = models.IntegerField(blank=True, null=True)
+    weight = models.FloatField(blank=True, null=True)
+    height = models.FloatField(blank=True, null=True)
+    shoe_size = models.FloatField(blank=True, null=True)
+    foot_width = models.FloatField(blank=True, null=True)
+    foot_length = models.FloatField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user_id + " " + self.age + " " + self.weight + " " + self.height
+
+
+class Test(models.Model):
+    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
@@ -31,13 +46,67 @@ class Test(models.Model):
         return self.name
 
 
-class SensorReadings(models.Model):
+class Step(models.Model):
     test_id = models.ForeignKey(Test, on_delete=models.CASCADE)
-    sensor_id = models.IntegerField()
     timestamp = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.timestamp + " " + self.value
+
+
+class SensorReading(models.Model):
+    step_id = models.ForeignKey(Step, on_delete=models.CASCADE)
+    sensor_id = models.IntegerField()
     value = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.sensor_id + " " + self.timestamp + " " + self.value
+
+
+class AverageStep(models.Model):
+    test_id = models.ForeignKey(Test, on_delete=models.CASCADE)
+    sensor_0_time = models.IntegerField()
+    sensor_0_value = models.FloatField()
+    sensor_1_time = models.IntegerField()
+    sensor_1_value = models.FloatField()
+    sensor_2_time = models.IntegerField()
+    sensor_2_value = models.FloatField()
+    sensor_3_time = models.IntegerField()
+    sensor_3_value = models.FloatField()
+    sensor_4_time = models.IntegerField()
+    sensor_4_value = models.FloatField()
+    sensor_5_time = models.IntegerField()
+    sensor_5_value = models.FloatField()
+
+    def __str__(self):
+        return (
+            self.test_id
+            + " "
+            + self.sensor_0_time
+            + " "
+            + self.sensor_0_value
+            + " "
+            + self.sensor_1_time
+            + " "
+            + self.sensor_1_value
+            + " "
+            + self.sensor_2_time
+            + " "
+            + self.sensor_2_value
+            + " "
+            + self.sensor_3_time
+            + " "
+            + self.sensor_3_value
+            + " "
+            + self.sensor_4_time
+            + " "
+            + self.sensor_4_value
+            + " "
+            + self.sensor_5_time
+            + " "
+            + self.sensor_5_value
+        )
