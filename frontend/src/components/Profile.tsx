5f8830078a8a25ef396490dfe09fbf53f6ec8cc8
snavemail <98936310+snavemail@users.noreply.github.com>
2024-03-14 16:45:39 -0400

    remove loading and add gender to profile

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
 import { Profile, User } from '../types';
+import { numberToGender } from '../utils';
 
-export default function ProfileDetails({
-  user,
-  profile,
-  loading,
-}: {
-  user: User;
-  profile: Profile;
-  loading: boolean;
-}) {
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
+export default function ProfileDetails({ user, profile }: { user: User; profile: Profile }) {
   if (!user || !profile) {
     return <div>User or profile not found</div>;
   }
@@ -23,6 +12,7 @@ export default function ProfileDetails({
       <h2>{user.first_name}</h2>
       <p>{user.email}</p>
       <p>{profile.dob}</p>
+      <p>{numberToGender(profile.gender)}</p>
     </div>
   );
 }
