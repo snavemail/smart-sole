22356f0725c41f3f23dd4086aa6dab04898cd6b9
snavemail <98936310+snavemail@users.noreply.github.com>
2024-03-19 00:13:30 -0400

    Initial Tests page for rendering all user tests

--- frontend/src/pages/userTests/UserTests.tsx
+++ frontend/src/pages/userTests/UserTests.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useProfile } from '../../hooks/useProfile';
+import './user-tests.css';
+import { Test } from '../../types';
+import TestComponent from '../../components/TestComponent';
 
 export default function UserTests() {
-  const { id } = useParams();
+  const { profile } = useProfile();
+  const [userTests, setUserTests] = useState([]);
 
-  return <div>UserTests</div>;
+  useEffect(() => {
+    const fetchUserTests = async () => {
+      try {
+        const response = await fetch(`http://127.0.0.1:8000/api/get-user-tests/${profile.id}`);
+        const data = await response.json();
+        setUserTests(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchUserTests();
+  }, [profile.id]);
+
+  return (
+    <div className='tests-page-container'>
+      <div className='header'></div>
+      <div className='testUserTests'>
+        {userTests.map((test: Test) => (
+          <TestComponent key={test.id} test={test} />
+        ))}
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/userTests/index.tsx
+++ frontend/src/pages/userTests/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import UserTests from './UserTests';
 
 export default function index() {
-  return <div>index</div>;
+  return <UserTests />;
 }
