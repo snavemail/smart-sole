7d7ab29e4b0c2fc445ce5989b92ca6f65b08c82d
snavemail <liam.jk.evans@gmail.com>
2024-03-24 19:01:47 -0400

    Remove bleapp and refactor files

--- bleproject/bleapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BleappConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "bleapp"
--- bleproject/settings.py
+++ bleproject/settings.py
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bleapp",
     "rest_framework",
     "rest_framework_simplejwt",
     "corsheaders",
