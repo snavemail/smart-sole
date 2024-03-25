1e0ff434557be768bbffc83b666c5653264a7032
snavemail <liam.jk.evans@gmail.com>
2024-03-25 08:46:00 -0400

    Update routers in routers.py

--- smartsole/routers.py
+++ smartsole/routers.py
@@ -1,18 +1,30 @@
-from rest_framework import routers
+from rest_framework_nested import routers
 from smartsole.user.viewsets import UserViewSet
 from smartsole.auth.viewsets import LoginViewSet, RegisterViewSet, RefreshViewSet
+from smartsole.gaittest.viewsets import GaitTestViewSet
+from smartsole.sensor.viewsets import SensorViewSet
 
 
-routers = routers.DefaultRouter()
+router = routers.SimpleRouter()
 
 # USER #
-routers.register(r"user", UserViewSet, basename="user")
+router.register(r"user", UserViewSet, basename="user")
 
 # AUTH #
-routers.register(r"auth/register", RegisterViewSet, basename="auth-register")
-routers.register(r"auth/login", LoginViewSet, basename="auth-login")
-routers.register(r"auth/refresh", RefreshViewSet, basename="auth-refresh")
+router.register(r"auth/register", RegisterViewSet, basename="auth-register")
+router.register(r"auth/login", LoginViewSet, basename="auth-login")
+router.register(r"auth/refresh", RefreshViewSet, basename="auth-refresh")
+
+# GAIT TEST #
+router.register(r"gaittest", GaitTestViewSet, basename="gaittest")
+
+router.register(
+    r"gaittest/(?P<gaittest_pk>[^/.]+)/sensor",
+    SensorViewSet,
+    basename="gaittest-sensor",
+)
+
 
 urlpatterns = [
-    *routers.urls,
+    *router.urls,
 ]
