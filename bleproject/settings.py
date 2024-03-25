9a84564d63ea1f9e089d75ff07c29eab230f3507
snavemail <liam.jk.evans@gmail.com>
2024-03-24 21:25:55 -0400

    Update CORS allowed origins and axios version

--- bleproject/settings.py
+++ bleproject/settings.py
@@ -133,10 +133,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
-
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
 
 AUTH_USER_MODEL = "smartsole_user.User"
