92ff271f760b092e6ee93ece96612db901fe48d5
snavemail <liam.jk.evans@gmail.com>
2024-03-26 06:21:50 -0400

    Add new utility functions and exports

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -2,5 +2,15 @@ import { getCircleStyles } from './heatmap';
 import { loadingStrings } from './loading';
 import { formatDate } from './formatdate';
 import { numberToGender } from './numberToGender';
+import { randomAvatar } from './randomAvatar';
+import { validateRegisterPage, validateLoginPage } from './validateField';
 
-export { getCircleStyles, loadingStrings, formatDate, numberToGender };
+export {
+  getCircleStyles,
+  loadingStrings,
+  formatDate,
+  numberToGender,
+  randomAvatar,
+  validateRegisterPage,
+  validateLoginPage,
+};
