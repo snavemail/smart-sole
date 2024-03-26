5cdaaf3a72cd710b95d123ffb00e3d63dec5c25c
snavemail <liam.jk.evans@gmail.com>
2024-03-25 21:25:55 -0400

    Add permissions for gaittest and gaittest-sensor

--- smartsole/auth/permissions.py
+++ smartsole/auth/permissions.py
@@ -1,22 +1,51 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
+# Used for gaittest and gaittest-sensor endpoints
 class UserPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            print("aaa")
-            return bool(request.user and request.user.is_authenticated)
-
+        if request.user and request.user.is_superuser:
+            return True
         if view.basename in ["gaittest"]:
-            print("bbb")
-            return bool(request.user and request.user.is_superuser)
-        print("ccc")
+            if request.method in SAFE_METHODS:
+                return bool(
+                    request.user
+                    and request.user.is_authenticated
+                    and obj.user == request.user
+                )
+        if view.basename in ["gaittest-sensor"]:
+            if request.method in ["DELETE"]:
+                return bool(
+                    request.user
+                    and request.user.is_authenticated
+                    and obj.gaittest.user == request.user
+                )
+            elif request.method in SAFE_METHODS:
+                return bool(
+                    request.user
+                    and request.user.is_authenticated
+                    and obj.gaittest.user == request.user
+                )
+
         return False
 
     def has_permission(self, request, view):
-        if view.basename in ["gaittest", "gaittest-sensor"]:
+        if request.user and request.user.is_superuser:
+            return True
+        if view.basename in ["gaittest"]:
             if request.method in SAFE_METHODS:
-                return bool(request.user and request.user.is_authenticated)
+                queryset = view.get_queryset()
+                if request.user and request.user.is_authenticated:
+                    user_gaittests = request.user.gaittest_set.all()
+                    filtered_queryset = queryset.filter(
+                        pk__in=[gaittest.pk for gaittest in user_gaittests]
+                    )
+                    return filtered_queryset.exists()
+        if view.basename in ["gaittest-sensor"]:
+            if request.method in SAFE_METHODS:
+                queryset = view.get_queryset()
+                if request.user and request.user.is_authenticated:
+                    user_gaittests = request.user.gaittest_set.all()
+                    return queryset.filter(gaittest__in=user_gaittests).exists()
 
-            return bool(request.user and request.user.is_authenticated)
         return False
