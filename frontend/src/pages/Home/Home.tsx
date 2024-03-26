65b832ceacb4be44e022549cd635f1e172a18a5f
snavemail <liam.jk.evans@gmail.com>
2024-03-26 06:21:41 -0400

    Refactor Home component

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { useUserActions } from '../../hooks/user.actions';
+import Navbar from '../../components/Navbar';
+import Layout from '../../components/Layout';
 
 export default function Home() {
   const userActions = useUserActions();
-  return (
-    <div>
-      <h1>Home</h1>
-      <button onClick={userActions.logout}>Logout</button>
-    </div>
-  );
+  return <div>Hello</div>;
 }
