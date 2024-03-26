8d927e6d914a9c28316dfd29e62f26aa55cec616
snavemail <liam.jk.evans@gmail.com>
2024-03-26 06:21:45 -0400

    Add Navbar component to LandingPage

--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Navbar from '../../components/Navbar';
 
 export default function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
