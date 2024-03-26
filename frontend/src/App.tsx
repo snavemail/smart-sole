5c18558128fa04f263972074fb3bdadf6e5dc1fb
snavemail <liam.jk.evans@gmail.com>
2024-03-26 06:20:28 -0400

    Add layout to routers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useBle } from './hooks/useBle';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import ProtectedRoute from './routes/ProtectedRoute';
 import UnAuthRoute from './routes/UnAuthRoute';
@@ -8,45 +8,48 @@ import Registration from './pages/AuthPages/Registration';
 import Login from './pages/AuthPages/Login';
 import Home from './pages/Home';
 import LandingPage from './pages/LandingPage/LandingPage';
+import Layout from './components/Layout';
 
 function App() {
   const { isConnected } = useBle();
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <UnAuthRoute>
-              <LandingPage />
-            </UnAuthRoute>
-          }
-        />
-        <Route
-          path='/register/'
-          element={
-            <UnAuthRoute>
-              <Registration />
-            </UnAuthRoute>
-          }
-        />
-        <Route
-          path='/login/'
-          element={
-            <UnAuthRoute>
-              <Login />
-            </UnAuthRoute>
-          }
-        />
-        <Route
-          path='/home'
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <UnAuthRoute>
+                <LandingPage />
+              </UnAuthRoute>
+            }
+          />
+          <Route
+            path='/register/'
+            element={
+              <UnAuthRoute>
+                <Registration />
+              </UnAuthRoute>
+            }
+          />
+          <Route
+            path='/login/'
+            element={
+              <UnAuthRoute>
+                <Login />
+              </UnAuthRoute>
+            }
+          />
+          <Route
+            path='/home'
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
