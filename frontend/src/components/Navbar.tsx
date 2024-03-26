f9d6866a3a89c6ebe2eb1f7c309ec46bb7ff4113
snavemail <liam.jk.evans@gmail.com>
2024-03-26 06:20:59 -0400

    Add navbar with dynamic buttons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,31 +1,64 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react';
+import { randomAvatar } from '../utils';
 import '../css/navbar.css';
+import { useNavigate } from 'react-router-dom';
+import { getUser } from '../hooks/user.actions';
 
-function Navbar() {
-  const location = useLocation();
+export default function Navbar() {
+  const { user } = getUser();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem('auth');
+    navigate('/login');
+  };
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
-  const hiddenNavbarRoutes = ['/search', '/add-user', '/search/', '/add-user/'];
+  const handleClickOutside = (event: MouseEvent) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+      setDropdownOpen(false);
+    }
+  };
 
-  const hideNavbar = hiddenNavbarRoutes.includes(location.pathname);
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('keydown', handleEscapeKey);
 
-  if (hideNavbar) {
-    return null;
-  }
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('keydown', handleEscapeKey);
+    };
+  }, []);
+
+  const handleEscapeKey = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      setDropdownOpen(false);
+    }
+  };
 
   return (
-    <div className='navbar-container'>
-      <a href='/profile'>
-        <div className='logo' />
-      </a>
-
-      <div className='links-container'>
-        <a href='/search'>Search</a>
-        <a href='/profile'>Profile</a>
-        <a href='/tests'>Tests</a>
+    <nav className='navbar'>
+      <div className='navbar-brand'>
+        <a href='/home'>
+          <h1>SENS</h1>
+        </a>
       </div>
-    </div>
+      {user && (
+        <div className='dropdown' onClick={toggleDropdown} ref={dropdownRef}>
+          <div className='circle-img'>
+            <img src={randomAvatar()} alt='Avatar' />
+          </div>
+          {dropdownOpen && (
+            <div className='dropdown-menu'>
+              <a href='/profile'>Profile</a>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          )}
+        </div>
+      )}
+    </nav>
   );
 }
-
-export default Navbar;
